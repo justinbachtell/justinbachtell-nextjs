2caeb05fb9285dd508b2bd543612d3279cc52f03
Justin Bachtell <justin@justinbachtell.com>
2024-05-23 12:48:21 -0500

    Add vercel analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,6 +88,7 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
             >
               {children}
               <SpeedInsights />
+              <Analytics />
             </ThemeProvider>
             <Toaster />
           </body>

5fda44a29399b1eee77639b8b04db5441091d218
Justin Bachtell <justin@justinbachtell.com>
2024-05-23 12:47:57 -0500

    Add vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { env } from "@/env.js";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata, Viewport } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@/styles/globals.css";
 
